9060763d920e087da028ca7c6c7f28208ef651aa
Grayson Mechler <Graysonmechler@gmail.com>
2020-01-06 23:34:06 -0600

    json update

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+app.listen(port, function() {
+  console.log("App listening on Port " + port);
+});
+
+app.get("/notes", function(re, res) {
+  res.sendFile(path.join(_dirname, "..public"));
+});
